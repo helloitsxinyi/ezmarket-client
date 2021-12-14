7a41388dccfd269132c9a26f0392f04076cbfef4
Xinyi <xinzwerkz@gmail.com>
2021-12-14 12:26:26 +0800

    modify model

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import {EzBackend, EzModel, Type} from '@ezbackend/common'
+import { EzBackend, EzModel, Type } from "@ezbackend/common";
 import { EzOpenAPI } from "@ezbackend/openapi";
 import { EzDbUI } from "@ezbackend/db-ui";
 import { EzCors } from "@ezbackend/cors";
 
-const app = new EzBackend()
+const app = new EzBackend();
 
 //---Plugins---
 //Everything is an ezapp in ezbackend
-app.addApp(new EzOpenAPI())
-app.addApp(new EzDbUI())
-app.addApp(new EzCors())
+app.addApp(new EzOpenAPI());
+app.addApp(new EzDbUI());
+app.addApp(new EzCors());
 //---Plugins---
 
 //Models are also ezapps in ezbackend
-const model = new EzModel('ModelName', {
-  var1: Type.VARCHAR, //string
-  var2: Type.INT, //integer
-})
+const model = new EzModel("Listing", {
+  shopName: Type.VARCHAR,
+  itemName: Type.VARCHAR,
+  price: Type.DOUBLE,
+  available: Type.BOOL,
+});
 
-app.addApp(model, { prefix: 'model-route-prefix' })
+app.addApp(model, { prefix: "model-route-prefix" });
 
-app.start()
+app.start();
