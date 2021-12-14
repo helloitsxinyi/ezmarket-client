f64461edfef8108eb827f3d5f3e0f6daff15b639
Xinyi <xinzwerkz@gmail.com>
2021-12-14 13:46:27 +0800

    install react-bootstrap and add navbar for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">FoodMarket</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Add Food Item</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
